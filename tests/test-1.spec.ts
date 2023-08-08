7f60e7f7c6d5dfba546d701cc61a5e9953fd8cc6
anamartpast <anamartinpastor1@gmail.com>
2023-08-08 20:52:18 +0200

    Style change in third game

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.goto('http://127.0.0.1:8080/home/home.html');
+  await page.goto('http://127.0.0.1:8080/index.html');
   await page.getByRole('link', { name: 'Jugar' }).getByRole('button').click();
 });
