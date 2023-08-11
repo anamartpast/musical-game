a2688ff606f47b584c09dcdd6c13bd45a8392d95
anamartpast <anamartinpastor1@gmail.com>
2023-08-11 22:31:22 +0200

    Update score system in third game

--- games/third_game.js
+++ games/third_game.js
@@ -1,22 +1,33 @@
 import MissingSeparatorStaff from "./missing_separator_staff.js";
 import { showMessage } from "../common/utils.js";
+import { updateScore, setScore, getScore } from "../common/score.js";
 
-// Partitura
-const staff = new MissingSeparatorStaff(document.querySelector('.grama-container'), { level: 3 });
 
 const thirdGame = {
+    nextLevelButton: document.querySelector("#next-level"),
+
     init: function() {
+        this.createStaff();
+        updateScore();
         const button = document.querySelector("#check");
         button.addEventListener('click', () => this.onSubmit());
-        staff.onResult((data) => this.onResult(data));
+        this.staff.onResult((data) => this.onResult(data));
+        this.nextLevelButton.addEventListener('click', () => this.onNextLevel())
+    },
+    createStaff: function() {
+        let level = this.staff?.options.level || 0;
+        level++;
+        
+        this.staff?.destroy();
+        this.staff = new MissingSeparatorStaff(document.querySelector('.grama-container'), { level: level });
     },
     onSubmit: function() {
-        if (staff.selectedSeparator === undefined) {
+        if (this.staff.selectedSeparator === undefined) {
             // TODO: "Tienes que mover el separador a su sitio correcto"
             return;
         }
 
-        staff.resolve();
+        this.staff.resolve();
     },
     onResult: function(success) {
         const msg = success ?
@@ -26,7 +37,21 @@ const thirdGame = {
             type: success ? 'success' : 'error',
             message: msg
         });
+
+        if (success) {
+            this.onSuccess();
+        }
+    },
+    onSuccess: function() {
+        setScore(getScore() + 10);
+        updateScore();
+        this.nextLevelButton.style.display = "initial";
+    },
+    onNextLevel: function() {
+        this.nextLevelButton.style.display = "none";
+        this.init();
     }
+    
 }
 
 thirdGame.init();
