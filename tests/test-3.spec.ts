40bf60a011dc168f521c340f41a9e75e564bac78
anamartpast <anamartinpastor1@gmail.com>
2023-08-21 21:21:42 +0200

    Updated tests

--- tests/test-3.spec.ts
+++ tests/test-3.spec.ts
@@ -3,4 +3,5 @@ import { test, expect } from '@playwright/test';
 test('test', async ({ page }) => {
   await page.goto('https://teoriamusical.info/');
   await page.getByRole('button', { name: 'Jugar' }).nth(2).click();
+  await page.close();
 });
