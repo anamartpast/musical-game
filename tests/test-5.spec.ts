0bf84245dd8b29d9dfced6f9d5be37a0209dcde0
anamartpast <anamartinpastor1@gmail.com>
2023-09-07 14:15:01 +0200

    Modify tests

---
+++ tests/test-5.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://teoriamusical.info/');
+  await page.getByRole('button', { name: 'Jugar' }).nth(1).click();
+  await expect(page.locator('.notes-container')).toHaveCount(1);
+  await page.close();
+});
